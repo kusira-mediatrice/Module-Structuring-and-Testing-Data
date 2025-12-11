79b45341c0992918f849f70ca47d49eba39373e0
kusira-mediatrice <kusiramul@gmail.com>
2025-12-11 05:39:17 +0100

    Install VSCode extensions + Implement short programs of Exercice 1

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -1,11 +1,14 @@
-let firstName = "Creola";
-let middleName = "Katherine";
-let lastName = "Johnson";
+let firstName = "Creola"; //declare variable firstName and assign it the string "Creola"
+let middleName = "Katherine"; //declare variable middleName and assign it the string "Katherine"
+let lastName = "Johnson"; //declare variable lastName and assign it the string "Johnson"
 
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
-
-let initials = ``;
-
+const index = 0;
+let initials = `${firstName.charAt(index)}${middleName.charAt(
+  index
+)}${lastName.charAt(index)}`;
+console.log(
+  `The initials of ${firstName} ${middleName} ${lastName} are ${initials}`
+);
 // https://www.google.com/search?q=get+first+character+of+string+mdn
-
