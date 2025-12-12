b00a321f3b5846540b12ab331e61d194de7b5a4d
kusira-mediatrice <kusiramul@gmail.com>
2025-12-12 04:43:55 +0100

    Complete excercise 1

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -1,7 +1,41 @@
 const minimum = 1;
 const maximum = 100;
 
-const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+const rand = Math.random();
+console.log("Math.random() =", rand);
+
+const range = maximum - minimum + 1;
+console.log("maximum - minimum + 1 =", range);
+
+const scaled = rand * range;
+console.log("Math.random() * (maximum - minimum + 1) =", scaled);
+
+const floored = Math.floor(scaled);
+console.log("Math.floor(Math.random() * (maximum - minimum + 1)) =", floored);
+const shifted = floored + minimum;
+
+console.log( "Math.floor(Math.random() * (maximum - minimum + 1)) + minimum =",shifted);
+
+const num = Math.floor(rand * (maximum - minimum + 1)) + minimum;
+console.log("Math.floor(Math.random() * (maximum - minimum + 1)) + minimum ="num);
+
+/*
+An example of calculating net salary from gross salary
+with a tax rate and a fixed deduction
+
+const salaryBrut = 4000;
+const taxRate = 25; // en pourcentage
+const deductionFixe = 300; // déduction fixe
+
+const irpf = (salaryBrut * taxRate) / 100;
+console.log("L'IRPF est de :", irpf);
+const deduction = irpf + deductionFixe;
+console.log("La déduction totale est de :", deduction);
+const salaryNet2 = salaryBrut - deduction;
+console.log("Le salaire net est de :", salaryNet2);
+
+const salaryNet = salaryBrut - ((salaryBrut * taxRate) / 100) - deductionFixe;
+console.log("Le salaire net est de :", salaryNet);*/
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
