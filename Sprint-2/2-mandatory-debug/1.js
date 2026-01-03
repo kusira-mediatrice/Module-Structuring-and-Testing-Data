06f9ee201294d05fa3acb4a3399fca076b03b810
kusira-mediatrice <kusiramul@gmail.com>
2026-01-03 14:01:38 +0100

    Update predictions and explanations in debugging exercises; correct function implementations for multiplication, summation, and last digit retrieval.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here   error because we return nothings and we write a code
+// after the return
 
 function sum(a, b) {
   return;
@@ -11,3 +12,9 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 // =============> write your explanation here
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum1(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum1(10, 32)}`);
