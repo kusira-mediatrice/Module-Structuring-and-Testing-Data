8fd60f72ef581f5e0d8ab3dc5bbbe1266fedfdc5
kusira-mediatrice <kusiramul@gmail.com>
2026-01-05 07:53:25 +0100

    Provide answers in time-format.js to clarify pad function usage and expected outputs

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -11,24 +11,26 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
+console.log(formatTimeDisplay(61));
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> write your answer here : pad is called 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> write your answer here :for the first time is the hour 0
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> write your answer here :for the first time is the hour 00
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> write your answer here : here we call pad with the parameter remainingSeconds that as the value 1
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> write your answer here :here pad return 01. num value is 1 which is on one position.
+// the function pad add 0 before 1 by using padStart function of string
